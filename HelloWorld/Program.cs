d1b0aa92969a5f9f1b4ff4a5effd702f1068704b
tayleredelen <taylerledelen@gmail.com>
2024-05-14 15:32:33 -0500

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//namespace in .NET framework 
+using System.Collections.Generic;
+//lists, collections, etc
+using System.Linq;
+//data
+using System.Text;
+//text, coding, etc
+using System.Threading.Tasks;
+//multithread applications
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) //start point of application
+            //paramaters (args) are optional
+            //return type is void, means no value returned it's just code
+            //case sensitive language, must be Main to be recognized
+        {
+            Console.WriteLine("Hello, World!");
+                //method is purple cube
+                //statements terminated by semi-colon
+
+        }
+    }
+}
